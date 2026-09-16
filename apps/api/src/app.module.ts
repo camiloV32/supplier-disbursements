8784a9b72e6b6c3c1993061eee77c3318b6337e0
Camilo <valenciaarcila2@gmail.com>
2026-09-15 23:03:49 -0500

    Modules creation

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule } from './config/app-config.module';
+import { SuppliersModule } from './modules/suppliers/infrastructure/suppliers.module';
+import { DisbursementsModule } from './modules/disbursements/infrastructure/disbursements.module';
 
 @Module({
   imports: [
-    AppConfigModule
+    AppConfigModule,
+    SuppliersModule,
+    DisbursementsModule
   ],
   controllers: [],
   providers: [],
